b994621b7c163aabe74292481507a9c3d2a81c3f
Roman Zipp <ich@ich.wtf>
2020-09-30 08:08:29 +0200

    Update config & service provider

--- config/blockade.php
+++ config/blockade.php
@@ -1,3 +1,45 @@
 <?php
 
-return [];
+return [
+
+    /**
+     * Decide if the authentication package should be enabled.
+     */
+    'enabled' => env('BLOCKADE_ENABLED', false),
+
+    /**
+     * Set the password used for the authentication process.
+     */
+    'password' => env('BLOCKADE_PASSWORD', null),
+
+    /**
+     * Specify the title displayed on the blockade view.
+     */
+    'title' => 'Under Construction',
+
+    /**
+     * Specify the authentication handler used for granting access.
+     *
+     * @see \romanzipp\Blockade\Handlers\HandlerContract
+     */
+    'handler' => \romanzipp\Blockade\Handlers\CookieHandler::class,
+
+    'handlers' => [
+
+        'cookie' => [
+
+            /**
+             * Set the cookie name used for the cookie handler.
+             */
+            'cookie' => env('BLOCKADE_COOKIE_NAME', 'blockade'),
+
+            /**
+             * Set the domain used for setting the cookie,
+             */
+            'domain' => env('BLOCKADE_COOKIE_DOMAIN', env('APP_URL')),
+
+        ],
+
+    ],
+
+];
--- src/Providers/BlockadeServiceProvider.php
+++ src/Providers/BlockadeServiceProvider.php
@@ -6,5 +6,27 @@ use Illuminate\Support\ServiceProvider;
 
 class BlockadeServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            dirname(__DIR__) . '/../config/blockade.php' => config_path('blockade.php'),
+        ], 'config');
+    }
 
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/../config/blockade.php', 'blockade'
+        );
+    }
 }
