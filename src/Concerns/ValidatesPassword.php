53a2f4dd3a1f45168317f1ce890761e76d474878
Roman Zipp <ich@ich.wtf>
2020-09-30 10:17:34 +0200

    Add password trait phpdoc

--- src/Concerns/ValidatesPassword.php
+++ src/Concerns/ValidatesPassword.php
@@ -4,16 +4,34 @@ namespace romanzipp\Blockade\Concerns;
 
 trait ValidatesPassword
 {
+    /**
+     * Check if a given plaintext password matches the configured password.
+     *
+     * @param string $password
+     * @return string
+     */
     protected function passwordMatchesConfigured(string $password): string
     {
         return $this->getPasswordHash($password) === $this->getPasswordHash(config('blockade.password'));
     }
 
+    /**
+     * Check if a given has matches the configured password hash.
+     *
+     * @param string $hash
+     * @return string
+     */
     protected function hashMatchesConfigured(string $hash): string
     {
         return $hash === $this->getPasswordHash(config('blockade.password'));
     }
 
+    /**
+     * Generate a hash from a given plaintext password.
+     *
+     * @param string $password
+     * @return string
+     */
     protected function getPasswordHash(string $password): string
     {
         return hash('sha256', $password);
