0d3110d60b52746294c013fb114032de2bab962a
Roman Zipp <ich@ich.wtf>
2020-11-20 08:39:59 +0100

    Add tailwind & eslint

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+        './resources/views/**/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
