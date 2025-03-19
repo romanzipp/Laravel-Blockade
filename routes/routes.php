86c10e526785a4a8c9a5e14fdad956c4ccca819a
romanzipp <romanzipp@users.noreply.github.com>
2025-03-19 05:32:34 +0000

    Fix styling

--- routes/routes.php
+++ routes/routes.php
@@ -2,8 +2,8 @@
 
 use romanzipp\Blockade\Http\Controllers\ValidatePasswordController;
 
-Route::prefix(config('blockade.routes.prefix'))
+\Route::prefix(config('blockade.routes.prefix'))
     ->middleware(config('blockade.routes.middleware'))
     ->group(function () {
-        Route::post('', ValidatePasswordController::class)->name('blockade.validate');
+        \Route::post('', ValidatePasswordController::class)->name('blockade.validate');
     });
